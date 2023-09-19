37bc4632775f98cc25ab8ba4900f595e69d028c2
joelnyongesa <joelnyongesa148@gmail.com>
2023-09-19 12:12:50 +0300

    An example of a decorator in action

---
+++ working_hours.py
@@ -0,0 +1,26 @@
+def check_working_hours(func):
+    def wrapper(time):
+        if 1100 < time < 2100:
+            func(time)
+        else:
+            print("I'm off duty!")
+    return wrapper
+
+@check_working_hours
+def sweep_floors(time):
+    print("Sweeping the floors...")
+
+@check_working_hours
+def wash_dishes(time):
+    print("Washing the dishes...")
+
+@check_working_hours
+def chop_vegetables(time):
+    print("Chopping the vegetables")
+
+
+
+# sweep_floors(1200)
+wash_dishes(1000)
+chop_vegetables(1200)
+sweep_floors(1600)
